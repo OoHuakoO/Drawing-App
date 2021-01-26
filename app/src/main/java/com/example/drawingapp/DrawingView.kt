3ac503c4f75d9138e9924d569fc256eecd94e596
huak <komsark_@hotmail.co.th>
2021-01-26 19:22:09 +0700

    OK

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,13 +1,16 @@
 package com.example.drawingapp
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.util.AttributeSet
 import android.view.View
 import java.nio.file.Path
 
-class DrawingView(context: Context,attrs: AttributeSet) : View(context,attrs) {
-private var mDrawPath : CustomPath? = null
-    internal inner class CustomPath(var color:Int,var brushThickness:Float) : Path(){
+class DrawingView(context: Context,attrs: AttributeSet) : View(context, attrs) {
+    private var mDrawPath : CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+        internal inner class CustomPath(var color:Int,
+                                        var brushThickness:Float) : android.graphics.Path(){
 
     }
 }
