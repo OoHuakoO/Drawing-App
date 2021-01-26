f81e1eaf7370f0f26ad07b67f6d67c1f64da12d6
huak <komsark_@hotmail.co.th>
2021-01-26 21:30:58 +0700

    OK

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,14 +1,34 @@
 package com.example.drawingapp
 
 import android.content.Context
-import android.graphics.Bitmap
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import java.nio.file.Path
+
 
 class DrawingView(context: Context,attrs: AttributeSet) : View(context, attrs) {
     private var mDrawPath : CustomPath? = null
     private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint : Paint? = null
+    private var mCanvasPaint : Paint? = null
+    private var mBrushSize : Float = 0.toFloat()
+    private var canvas : Canvas? = null
+
+    init{
+        setupDrawwing()
+    }
+    private fun setupDrawwing(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color,mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
+
+    private var color = Color.BLACK
         internal inner class CustomPath(var color:Int,
                                         var brushThickness:Float) : android.graphics.Path(){
 
